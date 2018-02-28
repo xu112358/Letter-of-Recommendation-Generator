74366d6ddec70eaa2c8e61bd56ceb8a485c65335
areke <markpklein@gmail.com>
2018-02-28 15:50:53 -0800

    start

--- letterOfRecGenerator/routes/recommender-dashboard.js
+++ letterOfRecGenerator/routes/recommender-dashboard.js
@@ -15,32 +15,46 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-    // nodemailer
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-            user: 'jsc6d5nbhjlppqts@ethereal.email',
-            pass: 'xZdzQ5HHMwNKN2esxS'
-        }
-    });
+    var gmailClass = google.gmail('v1');
 
-    // grab the email from the form
-    var email = req.body.email;
+    var email_lines = [];
+    var toEmail = req.body.email;
 
-    if (!email.length) {
+    if (!toEmail.length) {
         res.render('pages/recommender-dashboard', {
             title: 'RECOMMENDER DASHBOARD',
             statusMessage: 'Please provide a valid email'
         });
         return;
     }
+    email_lines.push('From: "test" <pcarion@gmail.com>');
+    email_lines.push('To: ' + email);
+    email_lines.push('Content-type: text/html;charset=iso-8859-1');
+    email_lines.push('MIME-Version: 1.0');
+    email_lines.push('Subject: Invitation to Fill Recommendation Letter Questionairre');
+    email_lines.push('And this would be the content.<br/>');
+    email_lines.push('The body is in HTML so <b>we could even use bold</b>');
+
+    var email = email_lines.join('\r\n').trim();
+    var base64EncodedEmail = new Buffer(email).toString('base64');
+    base64EncodedEmail = base64EncodedEmail.replace(/\+/g, '-').replace(/\//g, '_');
+
+    gmailClass.users.messages.send({
+      auth: auth,
+      userId: 'me',
+      resource: {
+        raw: base64EncodedEmail
+      }
+    }, cb);
+
+    // grab the email from the form
+    
 
     // setup email data with unicode symbols
     let mailOptions = {
         from: '<no-reply@example.com>', // sender address
         to: email, // list of receivers
-        subject: 'Invitation to Fill Recommendation Letter Questionairre', // Subject line
+        subject: '', // Subject line
         text: 'Please click the following questionairre link.', // plain text body
         html: '<p>Please click the following questionairre link.</p>' // html body
     };
