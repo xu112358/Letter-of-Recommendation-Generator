0e5aaad7247b1c557237adab8fc4463f56075213
Brandon Cen <bcen96@gmail.com>
2018-02-27 17:21:12 -0800

    tags

--- letterOfRecGenerator/public/javascripts/create-template.js
+++ letterOfRecGenerator/public/javascripts/create-template.js
@@ -4,10 +4,11 @@ var nextQuestionIdToUse = 0;
  * Prototype class for Questions
  */
 class Question {
-    constructor(type, value) {
+    constructor(type, value, tag) {
         // text, radio, checkbox
         this.type = type;
         this.value = value;
+        this.tag = tag;
         this.id = nextQuestionIdToUse;
         nextQuestionIdToUse++;
     }
@@ -18,7 +19,7 @@ const ADD_QUESTION_MODAL_ID = "add-question-modal";
 var questions = [];
 
 window.onload = function() {
-    questions.push(new Question("text", ""));
+    questions.push(new Question("text", "", ''));
     displayQuestions();
 }
 
@@ -41,10 +42,18 @@ function displayQuestions() {
 }
 
 function getQuestionHTML(q) {
-    var data_id_attribute = "data-id=\"" + q.id + "\"";    
-    return "<div class=\"question-container\">" +
-        "<textarea class=\"text-field\" type=\"text\" placeholder=\"Enter new question here...\" onkeyup=\"auto_grow(this)\"" + data_id_attribute + ">"
-        + q.value + "</textarea>" + "</div>";
+    var data_id_attribute = "data-id=\"" + q.id + "\""; 
+    var delete_onclick_attribute = "onclick=\"deleteQuestion(" + q.id + ")\"";   
+    return "<div class=\"question-outer-container\"" + data_id_attribute + ">" +
+                "<div class=\"question-container\">" +
+                    "<textarea data-type=\"value\" class=\"text-area underlined\" type=\"text\" placeholder=\"Enter new question here...\" onkeyup=\"auto_grow(this)\">"
+                    + q.value + "</textarea>" +
+                    "<span class=\"line\"></span>" +
+                    "<input data-type=\"tag\" class=\"text-field blue-text\" type=\"text\" placeholder=\"Enter answer tag here... (optional)\" value=\""
+                    + q.tag + "\">" +
+                "</div>" +
+                "<button class=\"question-button small-circle-button\" " + delete_onclick_attribute + ">X</button>" +
+            "</div>";
 }
 
 // used for allowing textareas to grow in height (trick with onkeyup)
@@ -60,6 +69,7 @@ function addQuestion() {
 
 function saveTemplate() {
     console.log("saveTemplate called");
+    updateQuestions();
 }
 
 function showAddQuestionModal() {
@@ -74,15 +84,29 @@ function hideAddQuestionModal() {
 
 function addTextAnswerQuestion() {
     console.log("addTestAnswerQuestion called");
-    updateQuestionValues();
-    questions.push(new Question("text", ""));
+    updateQuestions();
+    questions.push(new Question("text", "", ""));
     displayQuestions();
+    hideAddQuestionModal();
 }
 
-function updateQuestionValues() {
+function updateQuestions() {
     for (var i = 0; i < questions.length; i++) {
-        var query = "textarea[data-id='" + questions[i].id +"']";
-        var textarea = document.querySelector(query);
-        questions[i].value = textarea.value;
+        var query = "div[data-id='" + questions[i].id +"']";
+        var question = document.querySelector(query);
+
+        questions[i].value = question.querySelector("[data-type='value']").value;
+        questions[i].tag = question.querySelector("[data-type='tag']").value;
     }
+}
+
+function deleteQuestion(id) {
+    updateQuestions();
+    for (var i = 0; i < questions.length; i++) {
+        if (questions[i].id == id) {
+            questions.splice(i, 1);
+            break;
+        }
+    }
+    displayQuestions();
 }
