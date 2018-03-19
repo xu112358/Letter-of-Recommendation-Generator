00478764cae487dba2bcb229f043ae130ef7e7ab
Jonathan Frey <freyjona@usc.edu>
2018-03-19 01:14:57 -0700

    Letter preview now hooked up to backend

--- letterOfRecGenerator/public/javascripts/letter-preview.js
+++ letterOfRecGenerator/public/javascripts/letter-preview.js
@@ -1,50 +1,42 @@
+var id = parseAttribute('id');
+
 const ADD_QUESTION_MODAL_ID = "add-question-modal";
 const LETTER_CONTAINER_ID = "letter-container";
-const CONTENT_ID = "content";
 const TRIX_EDITOR = "trix-editor";
 const OUTER_CONTAINER = "outer-container";
+
+var form;
+var tagRegex = /\<\![a-z0-9_]+\>/ig;
 
 
 // eventually will be an array of letter content blocks (from backend)
 var state = 'Lorem ipsum this is a great student very good I love them wowza! WOWOFOFOAOSF TYPOOOO';
 
 var editable = [];
-var sections = ['This is a recommendation letter for Bob.<br><br>Bob is a great student. Super! Yes!<br><br>Okay now onto more paragraphs...','extracurriculars','<ul><li>bizarre talents include</li><li>juggling</li><li>underwater basket weaving</li></ul>','section 3','sEcTiOn 4',"click me! ! !<br><strong>I'mBOLD.<br></strong><em>ok</em>"];
+var sections = [];
 var curr_section;
 
-$('.example')
-  .form({
-    fields: {
-      content: {
-        identifier  : 'content',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Please enter a value'
-          }
-        ]
-      }
-    }
-});
-
-document.addEventListener("trix-change", function(event) {
-  //$('pre').html($(event.target).text());
-  //state = (event.target).text();
+document.addEventListener("trix-change", function (event) {
+    //$('pre').html($(event.target).text());
+    //state = (event.target).text();
 });
 
 // body
-function onLoad(){
-  // populate with the clicked text
-  var modal = document.getElementById(CONTENT_ID);
-  modal.setAttribute("value", state);
-
-  createLetterPreview();
-}
-
-// Renders innerHTML
-function renderLetterDisplay(){
-  var letterDisplayDiv = document.getElementById(LETTER_CONTAINER_ID);
-  letterDisplayDiv.innerHTML = state;
+function onLoad() {
+    $.ajax({
+        url: 'http://localhost:3000/letter-preview/form',
+        data: {id},
+        type: 'GET',
+        success: function (data) {
+            form = data;
+            console.log('success');
+            var letter = createLetterPreview(form);
+            sections.push(letter);
+        },
+        error: function () {
+            console.log('error');
+        }
+    });
 }
 
 function showEditModal(clicked) {
@@ -53,8 +45,8 @@ function showEditModal(clicked) {
     var modal = document.getElementById(ADD_QUESTION_MODAL_ID);
 
     var element = document.querySelector(TRIX_EDITOR);
-    console.log("SHOWEDITMODAL state: "+state);
-    console.log("SHOWEDITMODAL curr_section: "+curr_section);
+    console.log("SHOWEDITMODAL state: " + state);
+    console.log("SHOWEDITMODAL curr_section: " + curr_section);
     element.value = "";
     element.editor.insertHTML(state);
 
@@ -83,20 +75,38 @@ function cancelEditModal() {
 
 // Updates current section innerHTML
 function renderSelectedDisplay() {
-  var selectedDisplayDiv = document.getElementById(curr_section);
-  selectedDisplayDiv.innerHTML = state;
+    var selectedDisplayDiv = document.getElementById(curr_section);
+    selectedDisplayDiv.innerHTML = state;
 }
 
 // Creates the divs for each item in array
-function createLetterPreview() {
-  for (var s in sections) {
-      var newElement = document.createElement('div');
-      newElement.id = s; 
-      newElement.className = "letter-container";
-      newElement.innerHTML = sections[s];
-      newElement.onclick = function() {showEditModal(this.id);};
-      var outerContainer = document.getElementById(OUTER_CONTAINER);
-      outerContainer.appendChild(newElement);
-  }
+function createLetterPreview(form) {
+    var letter = document.createElement('div');
+    letter.id = '0';
+    letter.className = 'letter-container';
+    letter.innerHTML = parseLetter(form);
+    letter.onclick = function () {
+        showEditModal(this.id);
+    };
+    var outerContainer = document.getElementById(OUTER_CONTAINER);
+    outerContainer.appendChild(letter);
+
+    return letter.innerHTML;
 }
 
+function parseLetter(form) {
+    var letter = form.template.text;
+    var responses = form.responses;
+
+    return letter.replace(/(?:\r\n|\r|\n)/g, '<br />').replace(tagRegex, function (match) {
+        var response = responses.find(function (item) {
+            return item.tag.localeCompare(match, {sensitivity: 'base'}) == 0;
+        });
+
+        return response.response;
+    });
+}
+
+function parseAttribute(attr) {
+    return document.currentScript.getAttribute(attr) == '\'\'' ? null : document.currentScript.getAttribute(attr).replace(/['"]+/g, '');
+}
--- letterOfRecGenerator/routes/letter-preview.js
+++ letterOfRecGenerator/routes/letter-preview.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('pages/letter-preview', {
-        title: 'Letter Preview for ' + req.query.name,
+router.get('/', function (req, res, next) {
+    req.user.getForm(req.query.id, function (err, form) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('pages/letter-preview', {
+                title: 'Letter Preview for ' + form.email,
+                id: req.query.id
+            });
+        }
+    });
+});
+
+router.get('/form', function (req, res, next) {
+    req.user.getForm(req.query.id, function (err, form) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(form);
+        }
     });
 });
 
