ba23ff140fdafbafba8a618cc2dd03a34bfeb811
Matthew Asao Wenner <mwenner@usc.edu>
2018-04-10 23:37:46 -0700

    Seems to work

--- letterOfRecGenerator/public/javascripts/letter-preview.js
+++ letterOfRecGenerator/public/javascripts/letter-preview.js
@@ -117,13 +117,42 @@ function parseLetter(form) {
     var letter = form.template.text;
     var responses = form.responses;
 
-    return letter.replace(tagRegex, function (match) {
+    var noCapitalization = Array.from(letter.replace(tagRegex, function (match) {
         var response = responses.find(function (item) {
-            console.log(item.tag.localeCompare(match, {sensitivity: 'base'}) == 0);
             return item.tag.localeCompare(match, {sensitivity: 'base'}) == 0;
         });
         return response ? response.response : '';
-    });
+    }).replace(tagRegex, function (match) {
+        var response = responses.find(function (item) {
+            return item.tag.localeCompare(match, {sensitivity: 'base'}) == 0;
+        });
+        return response ? response.response : '';
+    }));
+
+    for(var i = 0; i < noCapitalization.length; i++) {
+
+        // Found ending punctuation that isn't the last letter in the text
+        if ((noCapitalization[i] == '.' || noCapitalization[i] == '?' || noCapitalization[i] == '!') && i != noCapitalization.length - 1) {
+
+            // Make sure exclamation point is not from a tag
+            if (noCapitalization[i] == '!' && i > 0 && noCapitalization[i - 1] == '<') {
+                continue;
+            }
+
+            // Look for the next alphabetical character to capitalize
+            var j = i + 1;
+            while (!((noCapitalization[j] >= 'a' && noCapitalization[j] <= 'z') || (noCapitalization[j] >= 'A' && noCapitalization[j] <= 'Z')) && j < noCapitalization.length) {
+                j++;
+            }
+
+            // Found character to capitalize
+            if (j < noCapitalization.length) {
+                noCapitalization[j] = noCapitalization[j].toUpperCase();
+            }
+        }
+    }
+
+    return noCapitalization.join("");
 }
 
 function parseAttribute(attr) {
