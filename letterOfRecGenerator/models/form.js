9403e379da641385cb3a26c838fc0c5688dbc6d4
Matthew Asao Wenner <mwenner@usc.edu>
2018-04-15 17:57:47 -0700

    Push empty string instead of null

--- letterOfRecGenerator/models/form.js
+++ letterOfRecGenerator/models/form.js
@@ -93,8 +93,8 @@ FormSchema.statics.submitForm = function (id, responseData, cb) {
             var responses = [];
             form['template']['questions'].forEach(function (question) {
                 var response = responseData[question.number - 1];
-
-                if (!response) {
+                
+                if (!response.length) {
                     responses.push({
                         tag: question.tag,
                         response: ''
