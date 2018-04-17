28f6270dffdb2093576ec7229ff087590d6ed7e3
Matthew Asao Wenner <mwenner@usc.edu>
2018-04-17 09:07:02 -0700

    localhost

--- letterOfRecGenerator/config/auth.js
+++ letterOfRecGenerator/config/auth.js
@@ -3,7 +3,7 @@ var OAuth2 = google.auth.OAuth2;
 
 var GOOGLE_CLIENT_ID = "468934162681-cbfmk6ung8pm2jaqsf68r9e24vb888j5.apps.googleusercontent.com";
 var GOOGLE_CLIENT_SECRET = "5BktU-GsMF8nf-SFmQFD7vSJ";
-var GOOGLE_CALLBACK = 'http://68.181.97.191:3000/auth/google/callback';
+var GOOGLE_CALLBACK = 'http://localhost:3000/auth/google/callback';
 
 var oauth2Client = new OAuth2(
   GOOGLE_CLIENT_ID,
