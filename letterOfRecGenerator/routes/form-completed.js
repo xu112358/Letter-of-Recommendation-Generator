3637aedba3333d3a1870d5687aa10619bc90bee1
Jerry Tejada <jmtejada@usc.edu>
2020-03-11 13:58:13 -0700

    Added email notification and changed time sort
    Added email notification feature and changed sorting on rec dashboard to newest to oldest

--- letterOfRecGenerator/routes/form-completed.js
+++ letterOfRecGenerator/routes/form-completed.js
@@ -1,11 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET form entry page. */
 router.get('/', function(req, res, next) {
     res.render('pages/form-completed', {
         title: 'Form Completed!',
     });
+
+    // EMAIL STUFF
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+          user: 'letterofrecgenerator@gmail.com', // generated ethereal user
+          pass: 'siqtam-3dabqa-pepxaV'  // generated ethereal password
+      },
+      tls:{
+        rejectUnauthorized:false
+      }
+    });
+
+    var email = req.user.email;
+    console.log("Email is: ", email);
+    console.log("Form data: ", req.body.responseData);
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Letter of Rec Generator" <letterofrecgenerator@gmail.com>', // sender address
+        to: email, // list of receivers
+        subject: 'Letter of Recommendation - Form Completed', // Subject line
+        text: req.body.email + ' has completed your recommendation request.', // plain text body
+        // html: '<p>' + req.body.body_text + ' ' + url + '</p>'// html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    });
+
 });
 
 module.exports = router;
