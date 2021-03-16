de6ca88d96ff6344c623cc28d5e3100b8152f81d
Zhiyu Liao <zhiyulia@usc.edu>
2021-03-17 00:46:43 +0800

    Update letter-preview.js

--- letterOfRecGenerator/routes/letter-preview.js
+++ letterOfRecGenerator/routes/letter-preview.js
@@ -91,6 +91,7 @@ router.post('/templateUpload', function (req,res, next) {
             } else {
 		console.log("\n\nForm:");
                 console.log(form);
+		form.letter = parseLetter(form);
                 pulled_text = form.letter;
 		    
 		console.log("\n\npulled_text:");
@@ -328,6 +329,50 @@ router.get('/downloads', function(req, res) {
     var file = path.resolve('./routes/uploads', 'output.docx');
     res.download(file);
 });
+
+function parseLetter(form) {
+    var tagRegex = /\<\![a-z0-9_]+\>/ig;
+    var letter = form;
+    var responses = form.responses;
+
+    var noCapitalization = Array.from(letter.replace(tagRegex, function (match) {
+        var response = responses.find(function (item) {
+            return item.tag.localeCompare(match, {sensitivity: 'base'}) == 0;
+        });
+        return response ? response.response : '';
+    }).replace(tagRegex, function (match) {
+        var response = responses.find(function (item) {
+            return item.tag.localeCompare(match, {sensitivity: 'base'}) == 0;
+        });
+        return response ? response.response : '';
+    }));
+
+    for (var i = 0; i < noCapitalization.length; i++) {
+
+        // Found ending punctuation that isn't the last letter in the text
+        if ((noCapitalization[i] == '.' || noCapitalization[i] == '?' || noCapitalization[i] == '!') && i != noCapitalization.length - 1) {
+
+            // Make sure exclamation point is not from a tag
+            if (noCapitalization[i] == '!' && i > 0 && noCapitalization[i - 1] == '<') {
+                continue;
+            }
+
+            // Look for the next alphabetical character to capitalize
+            var j = i + 1;
+            while (!((noCapitalization[j] >= 'a' && noCapitalization[j] <= 'z') || (noCapitalization[j] >= 'A' && noCapitalization[j] <= 'Z')) && j < noCapitalization.length) {
+                j++;
+            }
+
+            // Found character to capitalize
+            if (j < noCapitalization.length) {
+                noCapitalization[j] = noCapitalization[j].toUpperCase();
+            }
+        }
+    }
+
+    return noCapitalization.join("");
+
+}
 
 
 
