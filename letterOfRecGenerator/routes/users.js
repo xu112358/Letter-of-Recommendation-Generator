e0d54955641cf00a64fd018d50bb1c18951fe89e
Jared Kuo <jaredkuo@usc.edu>
2021-09-14 23:42:17 -0700

    Revert "fixed login db error"
    This reverts commit 2df5ddf6c507b373cd34ebae25ee429fa6dac2cb.

--- letterOfRecGenerator/routes/users.js
+++ letterOfRecGenerator/routes/users.js
@@ -16,9 +16,81 @@ router.get("/", function (req, res, next) {
   });
 });
 
+// Get Register page
+router.get("/register", (req, res) => res.render("register"));
+
 // Get Login page
 router.get("/login", (req, res) => res.render("login"));
 
+//Register handle
+router.post("/register", (req, res) => {
+  const { email, password, password2 } = req.body;
+  const linkTemplate_body = "Please click the following questionnaire ";
+  const linkTemplate_subject =
+    "Invitation to Fill Recommendation Letter Questionnaire";
+
+  let errors = [];
+
+  if (!email || !password || !password2) {
+    errors.push({ msg: "Please enter all fields" });
+  }
+
+  if (password != password2) {
+    errors.push({ msg: "Passwords do not match" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: "Password must be at least 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    // Validation passes
+    User.findOne({ email: email }).then((user) => {
+      if (user) {
+        errors.push({ msg: "Email already exists" });
+        res.render("register", {
+          errors,
+          email,
+          password,
+          password2,
+        });
+      } else {
+        const newUser = new User({
+          email,
+          password,
+          linkTemplate_subject,
+          linkTemplate_body,
+        });
+
+        console.log(newUser);
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then((user) => {
+                req.flash(
+                  "success_msg",
+                  "You are now registered and can log in"
+                );
+                res.redirect("/users/login");
+              })
+              .catch((err) => console.log(err));
+          });
+        });
+      }
+    });
+  }
+});
 
 // Login handle
 router.post("/login", (req, res, next) => {
@@ -46,27 +118,29 @@ router.post("/login", (req, res, next) => {
       console.log("user not in system");
       //use user's name to hash to password
       const newUser = new User({
-        email: userEmail,
-        password: '',
+        userEmail,
         name,
         linkTemplate_subject,
         linkTemplate_body,
       });
 
-  
-      newUser
-        .save()
-        .then((user) => {
-          req.login(user, function (err) {
-            if (err) {
-              return next(err);
-            }
-            return res.redirect("/recommender-dashboard");
-          });
-        })
-        .catch((err) => console.log(err));
-    
-      
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => {
+              req.login(user, function (err) {
+                if (err) {
+                  return next(err);
+                }
+                return res.redirect("/recommender-dashboard");
+              });
+            })
+            .catch((err) => console.log(err));
+        });
+      });
     }
   });
 });
