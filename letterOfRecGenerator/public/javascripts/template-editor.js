8f1fd92a97c103394a3d9c983139463046953cf0
Jared Kuo <jaredkuo@usc.edu>
2021-10-10 05:27:11 -0700

    front-end wip

--- letterOfRecGenerator/public/javascripts/template-editor.js
+++ letterOfRecGenerator/public/javascripts/template-editor.js
@@ -1,23 +1,72 @@
+let Embed = Quill.import('blots/embed');
+var quill = new Quill("#editor");
+
+class SpanEmbed extends Embed {
+  static create(value) {
+    const node = super.create();
+    node.classList.add('span-insert');
+    node.innerText = value;
+    return node;
+  }
+}
+
+window.onload = function() {
+  // Quill initialization
+  SpanEmbed.blotName = 'spanEmbed';
+  SpanEmbed.tagName = 'span';
+  Quill.register(SpanEmbed);
+};
+
 // Show options if checkbox or multiple choice is selected
 document.querySelector("form").addEventListener("change", (event) => {
-  console.log(event.target);
-  var select = event.target;
-  var value = select.options[select.selectedIndex].value;
-  if (value == "Multiple Choice" || value == "Checkboxes") {
-    var selectedCard = event.target.closest(".card");
-    selectedCard.querySelector(".options").classList.remove("d-none");
-  }
-  else {
-    var selectedCard = event.target.closest(".card");
-    selectedCard.querySelector(".options").classList.add("d-none");
+  // console.log(event.target);
+  
+  if (event.target.classList.contains("form-select")) {
+    var select = event.target;
+    var value = select.options[select.selectedIndex].value;
+    if (value == "Multiple Choice" || value == "Checkboxes") {
+      var selectedCard = event.target.closest(".card");
+      selectedCard.querySelector(".options").classList.remove("d-none");
+    }
+    else {
+      var selectedCard = event.target.closest(".card");
+      selectedCard.querySelector(".options").classList.add("d-none");
+    }
   }
 });
 
-// Add option
-document.querySelector(".add-options-btn").addEventListener("click", (event) => {
+// Handles click events inside form element
+document.querySelector("form").addEventListener("click", (event) => {
+ 
+  // Add option to question
+  if (event.target.classList.contains("add-options-btn")) {
+    addOption(event);
+  }
+
+  // Delete option from question
+  var targetElem = event.target.closest(".col-radio-delete");
+  if (targetElem) {
+    deleteOption(targetElem);
+  }
+
+  // Delete question from form
+  if (event.target.classList.contains("delete-question-icon")) {
+    console.log(event.target);
+    event.target.closest(".card").remove();
+  }
+});
+
+// Adds option element
+function addOption(event) {
+  // Check if there is only one option, if so, show delete button
+  var targetOptions = event.target.closest(".options");
+  var optionsCount = targetOptions.childElementCount - 1;
+  if (optionsCount == 1) {
+    targetOptions.querySelector(".col-radio-delete").classList.remove("d-none");
+  }
+
   // Find options div of current card
   var selectedOptions = event.target.closest(".options");
-  console.log(selectedOptions);
   
   var newOption = document.createDocumentFragment();
 
@@ -47,15 +96,20 @@ document.querySelector(".add-options-btn").addEventListener("click", (event) => 
   // Inserts new option before add option button
   var buttonRow = event.target.closest(".row");
   selectedOptions.insertBefore(newOption, buttonRow);
-});
+}
 
 // Delete option
-document.querySelector(".options").addEventListener("click", (event) => { 
-  var targetElem = event.target.closest(".col-radio-delete");
-  if (!targetElem) return;
-  targetElem.closest(".row").remove();
-
-});
+function deleteOption(targetElem) {
+  // Check if there is at least on option left, subtract one for options button
+  var targetOptions = targetElem.closest(".options");
+  var optionsCount = targetOptions.childElementCount - 1;
+  if (optionsCount > 1) {
+    targetElem.closest(".row").remove(); 
+    if (optionsCount == 2) {
+      targetOptions.querySelector(".col-radio-delete").classList.add("d-none");
+    }
+  }
+}
 
 // Add question
 document.querySelector(".add-questions-btn").addEventListener("click", (event) => {
@@ -63,6 +117,7 @@ document.querySelector(".add-questions-btn").addEventListener("click", (event) =
 
   var newQuestion = document.createDocumentFragment();
 
+  // Create question and dropdown
   var card = document.createElement("div");
   card.classList.add("card", "container", "align-middle");
 
@@ -93,8 +148,62 @@ document.querySelector(".add-questions-btn").addEventListener("click", (event) =
   option3.value = "Checkboxes";
   option3.innerHTML = "Checkboxes";
 
+  select.appendChild(option1);
+  select.appendChild(option2);
+  select.appendChild(option3);
+  
+  col2.appendChild(select);
+  col1.appendChild(ques_input);
+
+  question.appendChild(col1);
+  question.appendChild(col2);
+
+  // Create options section
+
   var options = document.createElement("div");
   options.classList.add("options", "d-none");
+
+  var row1 = document.createElement("row");
+  row1.classList.add("row");
+
+  var col_input = document.createElement("div");
+  col_input.classList.add("col-radio-input");
+
+  var opt_input = document.createElement("input");
+  opt_input.type = "text";
+  opt_input.classList.add("form-control");
+  opt_input.placeholder = "Option";
+
+  var col_delete = document.createElement("div");
+  col_delete.classList.add("col-radio-delete", "d-none");
+  
+  var del_option_icon = document.createElement("i");
+  del_option_icon.classList.add("fas", "fa-times");
+
+  var row2 = document.createElement("row");
+  row2.classList.add("row");
+
+  var col_input2 = document.createElement("div");
+  col_input2.classList.add("col-radio-input");
+
+  var add_opt_button = document.createElement("button");
+  add_opt_button.type = "button";
+  add_opt_button.classList.add("add-btn", "add-options-btn");
+  add_opt_button.innerHTML = "Add Option";
+
+  col_delete.appendChild(del_option_icon);
+  col_input.appendChild(opt_input);
+  
+  row1.appendChild(col_input);
+  row1.appendChild(col_delete);
+
+  col_input2.appendChild(add_opt_button);
+  row2.appendChild(col_input2);
+
+  options.appendChild(row1);
+  options.appendChild(row2);
+
+  // Creates tag section and question delete icon
 
   var tag = document.createElement("div");
   tag.classList.add("row");
@@ -111,17 +220,7 @@ document.querySelector(".add-questions-btn").addEventListener("click", (event) =
   col4.classList.add("col", "align-self-end");
 
   var icon = document.createElement("i");
-  icon.classList.add("fas", "fa-trash", "float-end");
-
-  select.appendChild(option1);
-  select.appendChild(option2);
-  select.appendChild(option3);
-  
-  col2.appendChild(select);
-  col1.appendChild(ques_input);
-
-  question.appendChild(col1);
-  question.appendChild(col2);
+  icon.classList.add("fas", "fa-trash", "float-end", "delete-question-icon");
 
   col4.appendChild(icon);
   col3.appendChild(tag_input);
@@ -136,10 +235,34 @@ document.querySelector(".add-questions-btn").addEventListener("click", (event) =
   form.appendChild(card);
 });
 
-//add eventListener to info icons
+// Tag insert
+document.querySelector(".tags").addEventListener("mouseup", (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains("tag")) {
+    console.log("click tag");
+    
+    quill.focus();
+    var range  = quill.getSelection();
+    if (range) {
+      console.log(range.index);
+      quill.insertEmbed(range.index, 'spanEmbed', " " + event.target.getAttribute('data-value') + " ");
+      quill.setSelection(range.index+1);
+    }
+  }
+});
+
+
+// Add eventListener to info icons
 document.getElementById("default-questions-info").onclick = function(event) {
 
   document.getElementById("exampleModalLabel").innerHTML = "Default Questions"
   document.getElementById("instructions").innerHTML = "Default questions are automatically added to the form and default tags are automatically created for the questions."
+  document.getElementById("open-modal").click();
+}
+
+document.getElementById("additional-questions-info").onclick = function(event) {
+
+  document.getElementById("exampleModalLabel").innerHTML = "Additional Questions"
+  document.getElementById("instructions").innerHTML = "Add additional custom questions to the form."
   document.getElementById("open-modal").click();
 }
