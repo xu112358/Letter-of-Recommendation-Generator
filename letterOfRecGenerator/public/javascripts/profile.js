8ac95ae55aebe9ac35f0ea298cb4e11b82fc4af6
Frost Tianjian Xu <xutianjian@gmail.com>
2021-10-17 23:26:48 -0700

    Implement address auto-completion feature.

--- letterOfRecGenerator/public/javascripts/profile.js
+++ letterOfRecGenerator/public/javascripts/profile.js
@@ -225,3 +225,77 @@ function updateProfile() {
     }
   });
 }
+
+// Address auto completion features.
+let autocomplete;
+
+function initAutocomplete() {
+  const address1Field = document.querySelector("#address1");
+  // Create the autocomplete object, restricting the search predictions to
+  // addresses in the US and Canada.
+  autocomplete = new google.maps.places.Autocomplete(address1Field, {
+    componentRestrictions: { country: ["us", "ca"] },
+    fields: ["address_components", "geometry"],
+    types: ["address"],
+  });
+  address1Field.focus();
+  // When the user selects an address from the drop-down, populate the
+  // address fields in the form.
+  autocomplete.addListener("place_changed", fillInAddress);
+}
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  const place = autocomplete.getPlace();
+  let address1 = "";
+  let postalCode = "";
+
+  // Get each component of the address from the place details,
+  // and then fill-in the corresponding field on the form.
+  // place.address_components are google.maps.GeocoderAddressComponent objects
+  // which are documented at http://goo.gle/3l5i5Mr
+  for (const component of place.address_components) {
+    const componentType = component.types[0];
+
+    switch (componentType) {
+      case "street_number": {
+        address1 = `${component.long_name} ${address1}`;
+        break;
+      }
+
+      case "route": {
+        address1 += component.short_name;
+        break;
+      }
+
+      case "postal_code": {
+        postalCode = `${component.long_name}${postalCode}`;
+        break;
+      }
+
+      case "postal_code_suffix": {
+        postalCode = `${postalCode}-${component.long_name}`;
+        break;
+      }
+      case "locality":
+        document.querySelector("#address3").value = component.long_name;
+        break;
+      case "administrative_area_level_1": {
+        document.querySelector("#address4").value = component.short_name;
+        break;
+      }
+      case "country":
+        break;
+    }
+  }
+
+  const address1Field = document.querySelector("#address1");
+  const address2Field = document.querySelector("#address2")
+  const postalField = document.querySelector("#postalCode");
+  address1Field.value = address1;
+  postalField.value = postalCode;
+  // After filling the form with address components from the Autocomplete
+  // prediction, set cursor focus on the second address line to encourage
+  // entry of subpremise information such as apartment, unit, or floor number.
+  address2Field.focus();
+}
