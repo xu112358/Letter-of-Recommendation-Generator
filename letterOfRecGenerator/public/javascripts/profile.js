277217f645bcaccdf4b5a1d3e19ca86b65fe95c1
ChenSir395 <minyiche@usc.edu>
2022-02-20 23:31:18 -0800

    regex token check

--- letterOfRecGenerator/public/javascripts/profile.js
+++ letterOfRecGenerator/public/javascripts/profile.js
@@ -50,10 +50,10 @@ $.ajax({
 function loadProfile() {
   var auth;
   var cookie = document.cookie.split(";");
+  const regex = " *auth=.+";
   for (var i = 0; i < cookie.length; i++) {
     if (
-      cookie[i].substring(0, 5) == "auth=" ||
-      cookie[i].substring(0, 6) == " auth="
+      cookie[i].match(regex)
     ) {
       auth = cookie[i];
     }
@@ -260,10 +260,12 @@ function updateProfile() {
   //load jwt token from cookie
   var auth;
   var cookie = document.cookie.split(";");
+  const regex = " *auth=.+";
   for (var i = 0; i < cookie.length; i++) {
     if (
-      cookie[i].substring(0, 5) == "auth=" ||
-      cookie[i].substring(0, 6) ==  " auth="
+      // cookie[i].substring(0, 5) == "auth=" ||
+      // cookie[i].substring(0, 6) ==  " auth="
+      cookie[i].match(regex)
     ) {
       auth = cookie[i];
     }
